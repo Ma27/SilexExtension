6901da3015d11b5075494161b9c0fdcf7de7ece5
Ma27 <ma27-se@hotmail.com>
2014-06-15 08:54:02 +0200

    Add kernel interface methods

--- lib/Kernel.php
+++ lib/Kernel.php
@@ -6,16 +6,49 @@ use Silex\Application;
 abstract class Kernel extends Application
 {
     private $env;
+    private $bundles = array();
     
     public function __construct($env, $debug = true)
     {
         parent::__construct(['debug' => (bool)$debug]);
         $this->env = (string)$env;
         
+        $this->setUpApplication();
+        $this->generateView();
+        $this->loadConfiguration();
+        
         $this->init();
+    }
+    
+    public function getEnv()
+    {
+        return $this->env;
+    }
+
+    public function getBundles()
+    {
+        return $this->bundles;
+    }
+    
+    protected function loadConfiguration()
+    {
+        
+    }
+    
+    protected function setUpApplication()
+    {
+        
+    }
+    
+    protected function generateView()
+    {
+        
     }
     
     protected function init()
     {
     }
+    
+    abstract public function registerBundles();
+    abstract public function getApplicationConfigPath();
 }
