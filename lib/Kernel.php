246cfebe197af89f8f01191b118efaf83d160c80
Ma27 <ma27-se@hotmail.com>
2014-06-15 15:03:19 +0200

    Added response generator and filter chain

--- lib/Kernel.php
+++ lib/Kernel.php
@@ -3,6 +3,9 @@ namespace Ma27\SilexExtension;
 
 use Silex\Application;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 abstract class Kernel extends Application
 {
@@ -105,18 +108,41 @@ abstract class Kernel extends Application
         });
         
         $this->on(KernelEvents::VIEW, function($event) use($app) {
-            $actionId = $app[Parameters::CURRENT_ACTION_ID];
+            $actionId   = $app[Parameters::CURRENT_ACTION_ID];
+            $attributes = array();
             foreach ($app[Parameters::HANDLER_STACK] as $handler) {
                 if (in_array($actionId, $handler->getSubscribedControllers())) {
-                    $handler->filterResponse($event);
+                    $list = $handler->filterResponse($event);
+                    
+                    $attributes = array_merge($attributes, (array)$list);
                 }
             }
             
             if ($event->hasResponse()) {
+                $event->setResponse($app->createResponse($event->getResponse()));
                 return;
             }
             
+            $template = $event->getRequest()->attributes->get(Parameters::TEMPLATE_KEY);
+            if (null === $template) {
+                $template = $event->getControllerResult();
+            }
             
+            try {
+                if (!isset($app[Parameters::TEMPLATE_ENGINE])) {
+                    throw new \Exception(null);
+                }
+                
+                $event->setResponse($app->createResponse(
+                    $app[Parameters::TEMPLATE_ENGINE]->render($template, $attributes)));
+                return;
+            } catch (\Exception $ex) {
+                if (null !== $app['logger'] && null === $ex->getMessage()) {
+                    $app['logger']->critical('Render exception occurred: ' . $ex->getMessage());
+                }
+                
+                $event->setResponse($app->createResponse($template, $attributes));
+            }
         });
     }
     
@@ -126,7 +152,24 @@ abstract class Kernel extends Application
     
     public function createResponse($output, array $additionHeaders = [])
     {
+        if ($output instanceof Response) {
+            foreach ($additionHeaders as $key => $value) {
+                $output->headers->set($key, $value);
+            }
+            return $output;
+        }
+        if ($output instanceof \SplFileInfo) {
+            return new BinaryFileResponse($output, $additionHeaders);
+        }
+        if (is_object($output) || is_array($output)) {
+            return new JsonResponse(json_encode($output, $additionHeaders));
+        }
         
+        json_decode($output);
+        if (json_last_error() === JSON_ERROR_NONE && !empty($output)) {
+            return new JsonResponse($output, $additionHeaders);
+        }
+        return new Response($output, $additionHeaders);
     }
     
     abstract public function registerBundles();
--- lib/Parameters.php
+++ lib/Parameters.php
@@ -5,4 +5,6 @@ final class Parameters
 {
     const HANDLER_STACK     = 'app.handlers';
     const CURRENT_ACTION_ID = 'app.controller.current_action';
+    const TEMPLATE_KEY      = 'result.params.template';
+    const TEMPLATE_ENGINE   = 'app.templating';
 }
